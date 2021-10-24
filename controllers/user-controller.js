391a5bda3a98bbbe32e7f39ecc6918436868a72f
Dylan Shoemaker <me@dylanshoemaker.com>
2021-10-23 18:50:13 -0700

    create and get users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,41 @@
-//placeholder
+const { User } = require("../models");
+
+const userController = {
+  getAllUser(req, res) {
+    User.find({})
+      // .populate({
+      //   path: "thoughts",
+      //   select: "-__v",
+      // })
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // get one User by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      //  .populate({
+      //      path: 'thoughts',
+      //      select: '-__v'
+      //   })
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // createUser
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = userController;
--- models/index.js
+++ models/index.js
@@ -1,1 +1,8 @@
-//placeholder
+const User = require('./User');
+const Thought = require('./Thought');
+
+
+
+
+
+module.exports = { User, Thought }; 
