9813fe39cd12381e4f383a1a96cb26da021650f8
Dylan Shoemaker <me@dylanshoemaker.com>
2021-10-23 17:44:52 -0700

    create user schema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,52 @@
-//placeholder
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: 'You need to provide a username!',
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: 'You need to provide an email!',
+      trim: true,
+    },
+  //   thoughts: [
+  //     {
+  //       type: Schema.Types.ObjectId,
+  //       ref: "Thought",
+  //     },
+  //   ],
+  //   friends: [
+  //     {
+  //       type: Schema.Types.ObjectId,
+  //       ref: "User",
+  //     },
+  //   ],
+  // },
+  // {
+  //   toJSON: {
+  //     virtuals: true,
+  //     getters: true,
+  //   },
+  //   id: false,
+  }
+);
+
+// get total count of comments and replies on retrieval
+// UserSchema.virtual("thoughtCount").get(function () {
+//   return this.thought.reduce(
+//     (total, thought) => total + thought.replies.length + 1,
+//     0
+//   );
+// });
+
+
+// create the User model using the UserSchema
+const User = model("User", UserSchema);
+
+// export the User model
+module.exports = User;
