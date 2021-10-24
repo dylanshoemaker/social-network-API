7f31eb8f858c78045d77f53557e58eb60f752e27
Dylan Shoemaker <me@dylanshoemaker.com>
2021-10-23 19:26:02 -0700

    create thoughts and reactions schemas

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,72 @@
-//placeholder
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+  {
+    //Reaction (SCHEMA ONLY)
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    commentBody: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+      ref: "User",
+    },
+
+    // use  reaction Schema to validate data for a reply
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of comments and replies on retrieval
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
